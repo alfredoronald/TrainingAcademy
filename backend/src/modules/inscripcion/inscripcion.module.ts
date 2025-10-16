a8e4e9bb3219e01f0c414158f969f43626901522
alfredoronald <choquerivealfredoronald@gmail.com>
2025-10-16 11:50:51 -0400

    feat: implement Inscripcion module with controller, service, entity, and DTO for enrollment management

--- backend/src/modules/inscripcion/inscripcion.module.ts
+++ backend/src/modules/inscripcion/inscripcion.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Inscripcion } from './inscripcion.entity';
-import { Curso } from '../curso/curso.entity';
-import { Usuario } from '../usuario/usuario.entity';
 import { InscripcionService } from './inscripcion.service';
 import { InscripcionController } from './inscripcion.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Inscripcion, Curso, Usuario])],
+  imports: [TypeOrmModule.forFeature([Inscripcion])],
   providers: [InscripcionService],
   controllers: [InscripcionController],
-  exports: [InscripcionService],
 })
 export class InscripcionModule {}
